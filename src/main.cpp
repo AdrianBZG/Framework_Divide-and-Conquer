d8fe979395922fdffc81935fec78972d4b7ef605
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2016-03-05 19:37:42 +0000

    Dummy main created

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/*
+    Author: Adrián Rodríguez Bazaga
+    Contact: arodriba@ull.edu.es / alu0100826456@ull.edu.es
+    Date: 05/03/2016
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+
+//Include the mandatory files to use the Framework
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "headers/Framework.h"
+//
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    Framework* framework = new Framework();
+    cout << "Hello" << endl;
+}
