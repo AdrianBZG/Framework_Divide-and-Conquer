c39648b99b670077e977caefcffdc9d261e0b195
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2016-03-05 20:20:41 +0000

    main.cpp updated

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,24 @@
 //Include the mandatory files to use the Framework
 #include "headers/Framework.hpp"
 //
+//Include the specific problems implementations
+#include "headers/FibonacciP.hpp"
+#include "headers/FibonacciS.hpp"
+//
 
 using namespace std;
 
 int main(int argc, char **argv) {
-    //Framework* framework = new Framework();
-    cout << "Hello" << endl;
+	if (argc != 2) {
+		cout << "\nIncorrect number of parameters. Found: " << argc-1 << ". Required: 1"<< endl;
+		exit(-1);
+	}
+
+	Problem* problem = new FibonacciP(atoi(argv[1]));
+	Solution* solution = new FibonacciS();
+	Framework* framework = new Framework();
+
+	framework->divideAndConquer(problem, solution);
+	cout << "\nResult:" << endl;
+	solution->solve();
 }
