a34954646efe76201cbb98ca0262decc2e9828a2
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2016-03-05 20:14:52 +0000

    Finished Problem.cpp implementation

---
+++ src/Problem.cpp
@@ -0,0 +1,23 @@
+/*
+    Author: Adrián Rodríguez Bazaga
+    Contact: arodriba@ull.edu.es / alu0100826456@ull.edu.es
+    Date: 05/03/2016
+*/
+
+#include "headers/Problem.hpp"
+
+Problem::Problem() {
+}
+
+Problem::~Problem() {
+}
+
+bool Problem::isSimple() {
+}
+
+pair<Problem*,Problem*> Problema::decompose() {
+}
+
+void Problem::simplySolve(Solution* s) {
+}
+
