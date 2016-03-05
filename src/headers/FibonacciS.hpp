d7ae249ba66037b9dd3d40b4c126fb1d3fd6de03
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2016-03-05 20:01:20 +0000

    Created Fibonacci test solution Header file

---
+++ src/headers/FibonacciS.hpp
@@ -0,0 +1,27 @@
+/*
+    Author: Adrián Rodríguez Bazaga
+    Contact: arodriba@ull.edu.es / alu0100826456@ull.edu.es
+    Date: 05/03/2016
+*/
+
+#ifndef _FIBONACCIS_HPP_
+#define _FIBONACCIS_HPP_
+
+#include "../framework/Solucion.h"
+
+class FibonacciS: public Solution {
+    private:
+	    int _n;
+	    
+    public:
+	    FibonacciS();
+	    virtual ~FibonacciS();
+
+	    void solve();
+	    void combine(pair<Solution*,Solution*>);
+	    Solution* getInstance();
+
+	    void setValue(int);
+};
+
+#endif /* _FIBONACCIS_HPP_ */
