21177990a57c36dbd96469d253877e6fdde19028
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2016-03-05 19:59:38 +0000

    Created Fibonacci test problem Header file

---
+++ src/headers/FibonacciP.hpp
@@ -0,0 +1,26 @@
+/*
+    Author: Adrián Rodríguez Bazaga
+    Contact: arodriba@ull.edu.es / alu0100826456@ull.edu.es
+    Date: 05/03/2016
+*/
+
+#ifndef _FIBONACCIP_HPP_
+#define _FIBONACCIP_HPP_
+
+#include "Problem.h"
+#include "FibonacciS.h"
+
+class FibonacciP: public Problem {
+    private:
+	    int _n;
+	    
+    public:
+	    FibonacciP(int);
+	    virtual ~FibonacciP();
+
+	    bool isSimple();
+	    pair<Problem*,Problem*> decompose();
+	    void simplySolve(Solution* s);
+};
+
+#endif /* _FIBONACCIP_HPP_ */
