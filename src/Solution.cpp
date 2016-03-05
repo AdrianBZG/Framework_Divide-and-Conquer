0c634cd2f13e4cca3c0747d6746b795138f38fd5
Adrián Rodríguez-Bazaga <adrian@scitex.eu>
2016-03-05 20:15:54 +0000

    Finished Solution.cpp implementation

---
+++ src/Solution.cpp
@@ -0,0 +1,22 @@
+/*
+    Author: Adrián Rodríguez Bazaga
+    Contact: arodriba@ull.edu.es / alu0100826456@ull.edu.es
+    Date: 05/03/2016
+*/
+
+#include "headers/Solution.hpp"
+
+Solution::Solution() {
+}
+
+Solution::~Solution() {
+}
+
+void Solution::solve() {
+}
+
+void Solution::combine(pair<Solution*,Solution*> subSolutions) {
+}
+
+Solution* Solution::getInstance() {
+}
